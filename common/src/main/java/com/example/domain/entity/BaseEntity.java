bd78d2b2b854db313a4a325cd1df325c4ff4a8a4
minh21012005 <baminhss@gmail.com>
2025-10-31 15:37:36 +0700

    fix: refactor base entity

--- common/src/main/java/com/example/domain/entity/BaseEntity.java
+++ common/src/main/java/com/example/domain/entity/BaseEntity.java
@@ -3,26 +3,33 @@ package com.example.domain.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
 
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 @Setter
 public abstract class BaseEntity<ID> {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private ID id;
 
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    @PrePersist
-    protected void onCreate() {
-        createdAt = LocalDateTime.now();
-    }
+    @CreatedBy
+    private String createdBy;
 
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
+    @LastModifiedBy
+    private String updatedBy;
 }
